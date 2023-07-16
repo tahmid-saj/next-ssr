f5ea6ac713ea171ceac8c81d95e9c66b606f797a
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-07-15 23:39:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <h1>SSR Magician</h1>
+    <Link href="/about">
+      <button>About</button>
+    </Link>
+    {/* <a href='/about'>About</a> */}
+  </div>
+);
+
+export default Index;
