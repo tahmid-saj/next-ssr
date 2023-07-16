b81e9b64ec6bd4962dc4c545a7b01c1b4d792a4b
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-07-16 00:06:28 -0400

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+const Robots = (props) => {
+  return (
+    <div>
+      <h1>Robots</h1>
+      <Link href="/">
+        <button>Home</button>
+      </Link>
+      <div>
+        {
+          props.robots.map(robot => (
+            <li key={robot.id}>
+              <Link href={`robots/${robot.id}`}>
+                <h1>{ robot.name }</h1>
+              </Link>
+            </li>
+          ))
+        }
+      </div>
+    </div>
+  )
+};
+
+Robots.getInitialProps = async function() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    robots: data
+  }
+}
+
+export default Robots;
